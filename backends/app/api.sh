56ca3c0e4cefc700ed0782cb1723e8464ca3e67e
JustGimli <aleksnermer@gmail.com>
2023-06-19 15:45:30 +0300

    add scripts

---
+++ backends/app/api.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+
+
+until python3 manage.py makemigrations
+do
+    echo "Waiting for db to be ready..."
+    sleep 3
+done
+
+until python3 manage.py migrate
+do
+    sleep 3
+done
+
+until python3 manage.py collectstatic --noinput
+do
+    sleep 3
+done
+
+
+
+
+gunicorn config.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4
