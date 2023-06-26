24db482a508be966e6e4177b67bc1c130d3017b1
JustGimli <aleksnermer@gmail.com>
2023-06-26 23:10:53 +0300

    add

---
+++ backends/asgi.py
@@ -0,0 +1,17 @@
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+from  api.v1.chats.sockets import websocket_urlpatterns
+
+
+
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": URLRouter(
+            websocket_urlpatterns
+        )
+    }
+)
