1503fbd6189d3a1c1ca09aee3f5c8aa131188e2e
JustGimli <aleksnermer@gmail.com>
2023-07-12 15:43:07 +0300

    Add

--- backends/app/config/urls.py
+++ backends/app/config/urls.py
@@ -3,17 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
-    # path('jet/', include('jet.urls', 'jet')),  # Django JET URLS
-    # path('jet/dashboard/', include('jet.dashboard.urls',
-    #      'jet-dashboard')),  # Django JET dashboard URLS
-    path('admin/', admin.site.urls),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
-    path('', include('api.v1.urls')),
+    path('api/', include('config.ur'))
 ]
-
-
-if settings.DEBUG:
-    urlpatterns = [path('__debug__/', include(
-        'debug_toolbar.urls'))] + urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
