1ee23193c312ba0eb60d0e88cb695de8fe43499f
JustGimli <aleksnermer@gmail.com>
2023-07-17 18:14:56 +0300

    Add

--- backends/app/api/v1/users/serializers.py
+++ backends/app/api/v1/users/serializers.py
@@ -5,8 +5,6 @@ from apps.users.models import User
 class UserSerializer(serializers.ModelSerializer):
     balance = serializers.DecimalField(
         max_digits=10, decimal_places=0, required=False)
-    user__id = serializers.DecimalField(
-        max_digits=10000000000, decimal_places=0, required=False)
 
     class Meta:
         model = User
