8d8c67c2282d2dad2b564fb2580d368383d9fc1e
JustGimli <aleksnermer@gmail.com>
2023-07-28 22:53:40 +0300

    add

--- backends/app/config/asgi.py
+++ backends/app/config/asgi.py
@@ -1,8 +1,12 @@
+from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+
 from api.v1.chats.sockets import websocket_urlpatterns
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from django.core.asgi import get_asgi_application
-django_asgi_app = get_asgi_application()
+
+
+
 
 
 application = ProtocolTypeRouter(
