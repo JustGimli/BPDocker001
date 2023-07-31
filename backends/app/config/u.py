afc5b1a1ee59ec202b45dbf4a28d8812b25be4fb
JustGimli <aleksnermer@gmail.com>
2023-07-31 20:53:46 +0300

    Add

--- backends/app/config/u.py
+++ backends/app/config/u.py
@@ -10,8 +10,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
-    path('', include('api.v1.urls')),
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', include('api.v1.urls'))
 ]
 
 
