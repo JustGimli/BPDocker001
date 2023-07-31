b8f84deaf7354d34d6ea028e3e7b9fc479ddc620
JustGimli <aleksnermer@gmail.com>
2023-07-31 20:10:41 +0300

    add

---
+++ backends/app/api.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+
+
+until python3 manage.py makemigrations
+do
+    echo "Waiting for db to be ready..."
+    sleep 3
+done
+
+until pip install channels[daphne]
+do
+    echo "Waiting for db to be ready..."
+    sleep 3
+done
+
+until python3 manage.py migrate
+do
+    sleep 3
+done
+
+until python3 manage.py collectstatic --noinput
+do
+    sleep 3
+done
+
+
+
+
+gunicorn config.wsgi --bind 0.0.0.0:8000 --workers 33 --threads 33
