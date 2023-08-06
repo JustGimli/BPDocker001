5fc0805d27c153e08c1697d77d4c25a7a19e343c
JustGimli <aleksnermer@gmail.com>
2023-08-06 21:01:13 +0300

    add

--- backends/app/config/u.py
+++ backends/app/config/u.py
@@ -13,3 +13,7 @@ urlpatterns = [
     path('', include('api.v1.urls')),
 ]
 
+
+if settings.DEBUG:
+    urlpatterns = [path('__debug__/', include(
+        'debug_toolbar.urls'))] + urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
